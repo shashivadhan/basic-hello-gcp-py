6827cda6395f7782137c584b6ca82e7a7f5c3087
shashivadhan <50444325+shashivadhan@users.noreply.github.com>
2025-04-15 15:14:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hello world'
+    return "Hello, Shashivadhan from GitHub to GCP!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
