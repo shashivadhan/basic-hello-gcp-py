2f53d49fb40ebb8b14092a6937ee6ddda9014b72
shashivadhan <50444325+shashivadhan@users.noreply.github.com>
2025-04-17 23:30:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+# main.py
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World from Cloud Run via GitHub!'
+    return 'hello_saikiran'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
